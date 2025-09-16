ac2db1362089f0bb6eba3d7166cb905695449d19
Botirjon Salokhiddinov <salokhiddinovbotirjon@gmail.com>
2025-09-16 15:00:43 +0200

    feat(api): convert /api/health to DRF view and keep al API routes under core.urls

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,7 +1,12 @@
-from django.urls import path 
-from .views import health
+from django.urls import path, include
+from .views import health, SubjectViewSet
 from . import views
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register("subjects", SubjectViewSet, basename= "subject")
 
 urlpatterns = [
     path("health/" , health, name = "health"),
+    path("", include(router.urls)),
 ]
--- backend/core/views.py
+++ backend/core/views.py
@@ -2,8 +2,16 @@ from django.shortcuts import render
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
+from rest_framework import viewsets, permissions
+from .models import Subject
+from .serializers import SubjectSerializer
 
 @api_view(["GET"])
 @permission_classes([AllowAny])
 def health(request):
     return Response({"ok": True, "app": "Edu Diary API"})
+
+class SubjectViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Subject.objects.all().order_by("name")
+    serializer_class = SubjectSerializer
+    permission_classes = [permissions.AllowAny]
